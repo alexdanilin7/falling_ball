5c6cc14d7755e960926a570642418207aacffd1e
DanilinAlexandr <danilin@yandexlyceum.ru>
2019-10-03 03:36:57 +0200

    готовый вариант

--- game.py
+++ game.py
@@ -8,6 +8,10 @@ clock = pygame.time.Clock()
 
 running = True
 screen2 = pygame.Surface(screen.get_size())
+
+vel = []
+ball_color = []
+
 x1, y1 = 0, 0
 start_ball = False
 y_pos = 0
@@ -25,14 +29,20 @@ while running:
             start_ball=True
             x, y_pos=event.pos
             r, g, b = random.randint(0,255), random.randint(0,255), random.randint(0,255)
-        #   pygame.draw.circle(screen, (255,255,255), event.pos, 10)
+            vel.append((int(x), int(y_pos)))
+            ball_color.append((r, g, b))
+
     screen.fill(pygame.Color("black"))
+    screen.blit(screen2, (0, 0))
     if start_ball:
-        pygame.draw.circle(screen, (r,g,b), (int(x), int(y_pos)), radius)
-        if y_pos<height-r:
-            y_pos += v / fps
-        else:
-            y_pos = height - r
+        for i in range(len(vel)):
+            x, y_pos = vel[i]
+            pygame.draw.circle(screen, ball_color[i], (int(x), int(y_pos)), radius)
+            if y_pos<height-radius:
+                y_pos+=v/fps
+            else:
+                y_pos=height-radius
+            vel[i]=(x, y_pos)
 
         clock.tick(fps)
 
